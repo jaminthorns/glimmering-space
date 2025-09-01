dbd1d3357f1647fe6f6fff48908a671929b430a4
Jamin Thornsberry <jamin.thorns@gmail.com>
2025-09-01 12:50:05 -0500

    Titles

--- lib/website_web/controllers/page_controller.ex
+++ lib/website_web/controllers/page_controller.ex
@@ -2,14 +2,20 @@ defmodule WebsiteWeb.PageController do
   use WebsiteWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    conn
+    |> assign(:page_title, "🌀")
+    |> render(:home)
   end
 
   def wither(conn, _params) do
-    render(conn, :wither)
+    conn
+    |> assign(:page_title, "_-' _-' _-'")
+    |> render(:wither)
   end
 
   def dissolve(conn, _params) do
-    render(conn, :dissolve)
+    conn
+    |> assign(:page_title, "... .. . .  .    .         .")
+    |> render(:dissolve)
   end
 end
